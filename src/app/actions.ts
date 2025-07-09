ec28fc25f99d38302099e03518793011be05b190
Patryk Niemczyk <patryk.niemczyk12345@gmail.com>
2025-07-09 16:16:18 +0200

    add server actions

---
+++ src/app/actions.ts
@@ -0,0 +1,58 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function checkIfCodeExists(formData: FormData) {
+  "use server";
+
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) {
+    throw new Error("DATABASE_URL environment variable is not set");
+  }
+
+  const sql = neon(databaseUrl);
+  const code = formData.get("code");
+
+  const result = await sql`SELECT * FROM urls WHERE code = ${code}`;
+  if (result.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export async function insertUrl(formData: FormData) {
+  "use server";
+
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) {
+    throw new Error("DATABASE_URL environment variable is not set");
+  }
+
+  const sql = neon(databaseUrl);
+  const longurl = formData.get("longurl");
+  const code = formData.get("code");
+
+  const existing = await sql`SELECT 1 FROM urls WHERE code = ${code}`;
+  if (existing.length > 0) {
+    throw new Error("Code already exists");
+  }
+
+  await sql`INSERT INTO urls (url, code) VALUES (${longurl}, ${code})`;
+}
+
+export async function getUrlFromCode(code: string) {
+  "use server";
+
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) {
+    throw new Error("DATABASE_URL environment variable is not set");
+  }
+
+  const sql = neon(databaseUrl);
+
+  const result = await sql`SELECT url FROM urls WHERE code = ${code}`;
+  if (result.length > 0) {
+    return result[0].url as string;
+  } else {
+    return null;
+  }
+}
